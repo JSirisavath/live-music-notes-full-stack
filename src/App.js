7f969d801a0fd265e50b644075b5e281a0abeb78
Jay <jerry.sirisavath@go.minneapolis.edu>
2024-02-21 19:57:30 -0600

    Working on implementing tailwind, but not working yet.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+// import './App.css';
+import './index.css';
+
+// Uncomment this to see tailwind styles working on browser.
+// import './output.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>START OF A GREAT PROJECT!</h1>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <h1 className="bg-blue-500 text-white p-4">
+          START OF A GREAT PROJECT! Testing Tailwind CSS!
+        </h1>
+      </div>
     </div>
   );
 }
