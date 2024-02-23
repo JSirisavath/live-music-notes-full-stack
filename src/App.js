d7e4c95820f2515b2042dd39b8c1e3942f3e62dd
Jay <jerry.sirisavath@go.minneapolis.edu>
2024-02-23 12:46:13 -0600

    Front end clean up in App.js, delete few uneccessary files to  get rid of error messages in console elements.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,67 @@
+import './assets/css/index.css';
+
 function App() {
   return (
-    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
-        </h2>
-      </div>
+    <div className="App">
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
 
-      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6" action="#" method="POST">
-          <div>
-            <label
-              for="email"
-              class="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Email address
-            </label>
-            <div class="mt-2"></div>
-          </div>
-
-          <div>
-            <div class="flex items-center justify-between">
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
               <label
-                for="password"
-                class="block text-sm font-medium leading-6 text-gray-900"
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Password
+                Email address
               </label>
-              <div class="text-sm">
-                <a
-                  href="#"
-                  class="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
+              <div className="mt-2"></div>
             </div>
-            <div class="mt-2"></div>
-          </div>
 
-          <div>
-            <button
-              type="submit"
-              class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2"></div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
-              Sign in
-            </button>
-          </div>
-        </form>
-
-        <p class="mt-10 text-center text-sm text-gray-500">
-          Not a member?
-          <a
-            href="#"
-            class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >
-            Start a 14 day free trial
-          </a>
-        </p>
+              Start a 14 day free trial
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "./assets/css/index.css";
+// import "./assets/css/index.css";
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Root is reference in HTML, and the " <App />" component is from App.js,  that is how our  react app is rendering.  No need to do  anything is this file. In other words,Index.html uses this file, and Index.js uses App.js for actual data.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* From App.js */}
     <App />
   </React.StrictMode>
 );
